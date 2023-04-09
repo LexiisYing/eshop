9132a470379316363d04c0d5208b520f7b6cb6ba
lexiisying <nyxxing0824@gmail.com>
2023-04-09 23:51:51 +0100

    ECom - Amazon clone

---
+++ src/Product.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Product.css';
+
+function Product({id, title, image, price, rating}) {
+  return (
+    <div>
+        <div className="product">
+            <div className="product__info">
+                <p>{title}</p>
+                <div className="product__price">
+                    <small>£</small>
+                    <strong>{price}</strong>
+                </div>
+                <div className="product__rating">
+                    {Array(rating)
+                    .fill()
+                    .map((_,i)=> (
+                        <p>⭐️</p>
+                    ))}
+                </div>
+            </div>
+
+            <img src={image} alt="product" />
+             <button>Add to Basket</button>
+        </div>
+    </div>
+  )
+}
+
+export default Product
